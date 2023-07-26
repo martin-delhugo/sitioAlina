b380e6294d03f5854f65eeb22cf8bff73686ba69
martin-delhugo <martindelhugo@mi.unc.edu.ar>
2023-07-26 04:46:50 -0300

    Prueba de fixeo nav mobile

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ const nav= document.querySelector("#nav1");
 const cerrar= document.querySelector("#cerrar");
 const abrir= document.querySelector("#abrir");
 
+
+
 abrir.addEventListener("click",()=>{
     nav1.classList.add("visible")
 })
@@ -11,22 +13,31 @@ cerrar.addEventListener("click",()=>{
 
 })
 
-function cerrar(){
-    nav1.classList.remove("visible");
+function cerr_inicio(){
+    console.log("cerrado")
+    nav1.classList.remove("visible")
+    location.href = "index.html"
+
+}
+function cerr_indicaciones(){
+    console.log("cerrado");
+    nav1.classList.remove("visible")
+    location.href = "indicaciones.html"
+
+}function cerr_contacto(){
+    console.log("cerrado")
+    nav1.classList.remove("visible")
+    location.href = "contacto.html"
 
 }
 
 /*  Funciones de redireccion de las secciones de inicio */
 function redir_indicaciones(){
-    nav1.classList.remove("visible");
-    console.log("CERRADO");
-    location.href = "indicaciones.html";
+    location.href = "indicaciones.html"
 }
 function redir_contacto(){
-    nav1.classList.remove("visible")
-    location.href = "contacto.html";
+    location.href = "contacto.html"
 }
 function redir_obras_sociales(){
-    nav1.classList.remove("visible");
     location.href = "indicaciones.html#obras-sociales"
 }
