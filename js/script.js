e07fe586387b06b4175a73d67db91ad235a79678
martin-delhugo <martindelhugo@mi.unc.edu.ar>
2023-07-26 03:58:46 -0300

    Prueba de fixeo de menu nav mobile

--- js/script.js
+++ js/script.js
@@ -13,11 +13,14 @@ cerrar.addEventListener("click",()=>{
 
 /*  Funciones de redireccion de las secciones de inicio */
 function redir_indicaciones(){
+    nav1.classList.remove("visible")
     location.href = "indicaciones.html"
 }
 function redir_contacto(){
+    nav1.classList.remove("visible")
     location.href = "contacto.html"
 }
 function redir_obras_sociales(){
+    nav1.classList.remove("visible")
     location.href = "indicaciones.html#obras-sociales"
 }
